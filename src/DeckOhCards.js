6e8bb08a3db34863022e54596e34b5ded48e3aa5
Crynowecer0 <crynowecer11@gmail.com>
2023-05-30 15:01:45 -0400

    complete part 1, pre review

--- src/DeckOhCards.js
+++ src/DeckOhCards.js
@@ -1,47 +1,58 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-/** shows current card and allows user to load another card */
-function DeckOhCards(){
-
+/** shows current card and allows user to load another card
+ *
+ * props: none
+ *
+ * state:
+ * - deck -> {data: null -> card deck object, isLoading: bool}
+ * - card - {code: str, image: str, images: {}, suit:str, value:str}
+ *
+ * App -> DeckOhCards
+ */
+function DeckOhCards() {
   const [deck, setDeck] = useState({
     data: null,
-    isLoading: true
-  })
-  const [card, setCard] = useState(false)
+    isLoading: true,
+  });
+  const [card, setCard] = useState(false);
 
-  useEffect(()=>{
-    async function getDeck(){
-      //ajax call for the deck that we will await
-      const newDeck = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
-      //setDeck to the new deck
+  useEffect(() => {
+    async function getDeck() {
+      const newDeck = await axios.get(
+        "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+      );
       setDeck({
         data: newDeck.data,
-        isLoading: false
-      })
+        isLoading: false,
+      });
     }
     getDeck();
-  }, [])
+  }, []);
 
-  async function getNewCard(){
-    if (deck.data.remaining < 1) {
+  /**get a single card from the deck if one is available */
+  async function getNewCard() {
+    let newCard = await axios.get(
+      `https://deckofcardsapi.com/api/deck/${deck.data.deck_id}/draw/?count=1`
+    );
+
+    if (!newCard.data.remaining) {
       window.alert("Error: No cards remaining!");
-      return ""
+      console.log(deck.data.remaining);
+      return;
     }
-    //otherwise, make the request to the api with the deck id
-    let newCard = await axios.get(`https://deckofcardsapi.com/api/deck/${deck.data.deck_id}/draw/?count=1`);
-    console.log("resp object is>", newCard.data);
     setCard(newCard.data.cards[0].image);
   }
 
+  if (deck.isLoading) return <h1>Loading....</h1>;
 
-if (deck.isLoading) return <h1>Loading....</h1>
-
-return (
-  <div>
-    {card ? <img src={card} /> : <h2>Click to begin!</h2>}
-    <button onClick={getNewCard}>Click for card!</button>
-  </div>
-)
+  return (
+    <div>
+      {card ? <img src={card} alt="card" /> : <h2>Click to begin!</h2>}
+      <button onClick={getNewCard}>Click for card!</button>
+    </div>
+  );
 }
+
 export default DeckOhCards;
